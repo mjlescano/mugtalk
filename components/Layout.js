e3f6a71513c61601653e67b76091cbac53d2df44
Matías Lescano <mjlescano@protonmail.com>
2016-12-08 15:20:57 -0300

    update layout declaration

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
 
-export default (props) => (
+export default ({ children }) => (
   <div>
     <Head>
       <title>MugTalk.</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    {props.children}
+    { children }
   </div>
 )
