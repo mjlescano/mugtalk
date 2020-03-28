903755940d2a962c64b8e11a64f47aec2f4aa570
Matías Lescano <mjlescano@protonmail.com>
2020-03-28 17:29:27 -0300

    Fix styles

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app'
-import '../styles.css'
+import '../styles/_app.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App ({ Component, pageProps }: AppProps) {
